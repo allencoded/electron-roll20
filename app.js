75428289e52121d86a8df75792b36bc2c4d0e3be
Allen Hendricks <allencoded@gmail.com>
2016-02-17 01:52:47 -0500

    LastMainWindow state is now saved. It will restore the window to the size it was when it was last closed.

--- app.js
+++ app.js
@@ -3,17 +3,42 @@
 var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = require('browser-window');
+var storage = require("./libs/storage");
 
 app.on('ready', function () {
 
+  // grab last main window state
+  var lastMainWindowState = storage.get("lastMainWindowState");
+  if (lastMainWindowState === null) {
+    lastMainWindowState = {
+      width: 800,
+      height: 600,
+      maximized: false
+    };
+  }
+
   var mainWindow = new BrowserWindow ({
-    width: 800,
-    height: 600,
+    x: lastMainWindowState.x,
+    y: lastMainWindowState.y,
+    width: lastMainWindowState.width,
+    height: lastMainWindowState.height,
     webPreferences: {
       webSecurity: false
     }
   });
-
+  
   mainWindow.loadURL('https://roll20.net/');
   //mainWindow.webContents.openDevTools();
+
+  mainWindow.on('close', function() {
+    var bounds = mainWindow.getBounds();
+    storage.set("lastMainWindowState", {
+      x: bounds.x,
+      y: bounds.y,
+      width: bounds.width,
+      height: bounds.height,
+      maximized: mainWindow.isMaximized()
+    });
+  });
+
 });
