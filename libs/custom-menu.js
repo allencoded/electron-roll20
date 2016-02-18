a80be026438da823c1fd1ecb2c10d33c8e9ccf10
Allen Hendricks <allencoded@gmail.com>
2016-02-17 23:17:58 -0500

    Added View items

--- libs/custom-menu.js
+++ libs/custom-menu.js
@@ -1,3 +1,6 @@
+const remote = require('electron').remote;
+const BrowserWindow = require('electron').BrowserWindow;
+
 exports.getTemplate = function () {
   var template = [
     {
@@ -10,8 +13,41 @@ exports.getTemplate = function () {
           }
         }
       ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Toggle Fullscreen',
+          accelerator: 'F11',
+          click: function (item, focusedWindow) {
+            if (focusedWindow){
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+            }
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: (function() {
+          if (process.platform == 'darwin')
+            return 'Alt+Command+I';
+          else
+            return 'Ctrl+Shift+I';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.toggleDevTools();
+            }
+          },
+        {
+          label: 'Reload',
+          accelerator: 'F5',
+          click: function () {
+            BrowserWindow.getFocusedWindow().reloadIgnoringCache();
+          }
+        }
+      ]
     }
   ];
-
   return template;
-}
+};
